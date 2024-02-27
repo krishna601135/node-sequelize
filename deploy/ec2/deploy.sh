13d40a12c9fe2d7df0fe78ec78fdd0c7bcd27e50
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-02-27 15:34:53 +0530

    added deploy stage in gitlab-ci.yml file and added deploy folder

---
+++ deploy/ec2/deploy.sh
@@ -0,0 +1,16 @@
+wget pemfile (#download the pem file using wget package)
+
+chmod 400 pem (give permission)
+
+#connect to dev server using ssh
+ssh -i 
+
+ecr login
+
+remove previous running containers
+
+docker pull image from Ecr
+
+#download the image from the registry and containerize
+docker run
+
