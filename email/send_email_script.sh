1636cab3886722df84f9697361ba98eade1eadf1
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-07 15:41:11 +0530

    added sendmail job in gitlab file

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Define email content and recipients
-EMAIL_SUBJECT="Vulnerabilities Found in Container Scanning"
-EMAIL_BODY="Vulnerabilities have been detected in the container scanning stage. Please review the security report."
+EMAIL_SUBJECT="Container Scanning Report"
+RECIPIENT="krishnakakarapathi@gmail.com"
 
-# Send email using the 'mail' command
-mail -s "$EMAIL_SUBJECT" kakarapathikrishna@gmail.com <<< "$EMAIL_BODY"
+# Construct the email message
+EMAIL_BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
+
+# Send email with attachment
+echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" -a container-scanning-report.json "$RECIPIENT"
