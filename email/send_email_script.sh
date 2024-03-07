dbb041a8993d2d822a5f9b7c5f6bfbaa88913193
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-07 16:02:57 +0530

    changes in gitlab file and send_email_script

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -4,8 +4,11 @@
 EMAIL_SUBJECT="Container Scanning Report"
 RECIPIENT="krishnakakarapathi@gmail.com"
 
+# Download the artifact using wget
+wget -O container-scanning-report.json "${CI_JOB_URL}/artifacts/raw/container-scanning-report.json"
+
 # Construct the email message
 EMAIL_BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
 
 # Send email with attachment
-echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" -a container-scanning-report.json "$RECIPIENT"
+echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" -a "container-scanning-report.json" "$RECIPIENT"
