1bac4a46fa1284cd1dda7dac79a81cde4e5b52f8
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-11 16:43:34 +0530

    changes in upload.ts

--- reports/upload.ts
+++ reports/upload.ts
@@ -1,53 +1,37 @@
+import * as fs from "fs";
 import mongoose from "mongoose";
-import fs from "fs";
 
-// Collection name
+const uri = process.env.MONGO_URL || ""; // Default to empty string if MONGO_URL is not defined
 const collectionName = "reports";
 
-// Define Mongoose schema
 const reportSchema = new mongoose.Schema({
   jsonData: Object,
 });
 
 const Report = mongoose.model(collectionName, reportSchema);
 
-// Function to upload JSON report to MongoDB
 async function uploadToMongoDB(filePath: string, mongourl: string) {
   try {
-    // Read the JSON report file
     const reportData = JSON.parse(fs.readFileSync(filePath, "utf8"));
-
-    // Connect to MongoDB
-    await mongoose.connect(mongourl);
-
-    // Insert the report data into MongoDB using Mongoose
+    await mongoose.connect(mongourl); // Use mongourl instead of uri
     const newReport = new Report({ jsonData: reportData });
-
-    // Save the new report to MongoDB
     const result = await newReport.save();
-
     console.log(`Report uploaded to MongoDB with ID: ${result._id}`);
-
-    // Close the MongoDB connection
     await mongoose.disconnect();
   } catch (error) {
     console.error("Error uploading report to MongoDB:", error);
   }
 }
 
-// Check if the file path argument is provided
 const filePath = process.argv[2];
 if (!filePath) {
-  console.error("Usage: ts-node uploadToMongoDB.ts <file_path>");
+  console.error("Usage: node uploadToMongoDB.js <file_path>");
   process.exit(1);
 }
 
-// Get the MongoDB URL from environment variable
-const mongoUrl = process.env.MONGO_URL;
-if (!mongoUrl) {
+if (!uri) {
   console.error("MongoDB URL not provided in environment variables.");
   process.exit(1);
 }
 
-// Upload the report to MongoDB
-uploadToMongoDB(filePath, mongoUrl);
+uploadToMongoDB(filePath, uri);
