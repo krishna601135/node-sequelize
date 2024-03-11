a7bbeb416ad9aa825ad1f8adf83773b47b3cfc32
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-11 15:29:00 +0530

    changed mongodb installation command in send_email and hardcoded mongouri in upload.js

--- reports/upload.js
+++ reports/upload.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-let filePath = process.argv[2];
 
-let mongourl = process.argv[3];
 
+const uri = "mongodb+srv://library:saimohanlib@cluster0.k1eoilz.mongodb.net/online-library"
 
 // Collection name
 const collectionName = "reports";
@@ -23,7 +22,7 @@ async function uploadToMongoDB(filePath, mongourl) {
      const reportData = JSON.parse(fs.readFileSync(filePath, "utf8"));
     
      // Connect to MongoDB
-    await mongoose.connect(mongourl, () => console.log('Mongo DB Connected!!'));
+    await mongoose.connect(uri, () => console.log('Mongo DB Connected!!'));
 
     // Insert the report data into MongoDB using Mongoose
     const newReport = new Report({ jsonData: reportData });
@@ -41,11 +40,11 @@ async function uploadToMongoDB(filePath, mongourl) {
 }
 
 // Check if the file path argument is provided
-// const filePath = process.argv[2];
+const filePath = process.argv[2];
 if (!filePath) {
   console.error("Usage: node uploadToMongoDB.js <file_path>");
   process.exit(1);
 }
 
 // Upload the report to MongoDB
-uploadToMongoDB(filePath, mongourl);
+uploadToMongoDB(filePath);
