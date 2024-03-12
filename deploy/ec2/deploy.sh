02b864a3a08e98bfa81db6616ba2233088be70ed
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-12 15:22:24 +0530

    ecr pull changed and new send_email_script.ts added

--- deploy/ec2/deploy.sh
+++ deploy/ec2/deploy.sh
@@ -7,22 +7,22 @@ else
     chmod 400 $UBUNTU_KEYPAIR
 
     # Connect to dev server using SSH
-    ssh -i UBUNTU_KEYPAIR ubuntu@3.91.229.191
+    ssh -i $UBUNTU_KEYPAIR ubuntu@18.212.200.65
 
     #login into root user
     sudo -i
 
     # Login to Amazon ECR (Elastic Container Registry)
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 680866457108.dkr.ecr.us-east-1.amazonaws.com
-    
+
     # Remove previous running containers (if necessary)
     docker container prune -f
 
     # Pull the image from Amazon ECR
-    docker pull 680866457108.dkr.ecr.us-east-1.amazonaws.com/library:latest
+    docker pull 680866457108.dkr.ecr.us-east-1.amazonaws.com/libtool:latest
 
     # Run the container
-    docker run -d -p 5000:4001 680866457108.dkr.ecr.us-east-1.amazonaws.com/library:latest
+    docker run -d -p 5000:4001 680866457108.dkr.ecr.us-east-1.amazonaws.com/libtool:latest
 fi
 
 
--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-# Define email content and recipients
-EMAIL_SUBJECT="Container Scanning Report"
+######################
+#Author: Mohan
+#Date: 12/03/2024
+#This script fetches latest json file from reports collection and sends attachment to mail
+####################
 
-RECIPIENT="krishnakakarapathi@gmail.com"
 
-#  curl --location --header "PRIVATE-TOKEN: 9koXpg98eAheJpvBs5tK" "https://gitlab.example.com/api/v4/projects/1/jobs/8/artifacts"
 
-# Download the artifact using wget
-wget -O container-scanning-report.json "https://gitlab.com/gitlabworkflow/online-library/-/jobs/6363017609/artifacts/raw/container-scanning-report.json"
+curl https://downloads.mongodb.com/compass/mongodb-mongosh_2.1.5_amd64.deb
 
-# Construct the email message
-EMAIL_BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
 
-# Send email with attachment
-echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" "$RECIPIENT" -a "container-scanning-report.json"
+mongo --version
+
+
+
+# # Define email content and recipients
+# EMAIL_SUBJECT="Container Scanning Report"
+
+# RECIPIENT="krishnakakarapathi@gmail.com"
+
+# #  curl --location --header "PRIVATE-TOKEN: 9koXpg98eAheJpvBs5tK" "https://gitlab.example.com/api/v4/projects/1/jobs/8/artifacts"
+
+# # Download the artifact using wget
+# wget -O container-scanning-report.json "https://gitlab.com/gitlabworkflow/online-library/-/jobs/6363017609/artifacts/raw/container-scanning-report.json"
+
+# # Construct the email message
+# EMAIL_BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
+
+# # Send email with attachment
+# echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" "$RECIPIENT" -a "container-scanning-report.json"
 
 
