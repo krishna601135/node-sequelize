2b713c0cf7062a081854fe6c4eab389c657d6a92
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-12 17:05:45 +0530

    added installation direclty in send_email

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -7,29 +7,6 @@
 
 
 
-sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates
-
-sudo apt install -y mongodb
-
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-
-deb [arch=amd64,arm64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse
-
-sudo apt update
-
-
-sudo apt install -y mongodb-org
-
-mongod --version
-
-sudo systemctl status mongod
-
-sudo systemctl start mongod
-
-sudo systemctl status mongod
-
 
 
 
