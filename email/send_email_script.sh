deb73927130f468ca7268ccce1354fd0f83492cd
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-12 17:23:44 +0530

    added mail sending steps in send_email_script.sh

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -1,4 +1,3 @@
-
 ######################
 #Author: Mohan
 #Date: 12/03/2024
@@ -6,25 +5,28 @@
 ####################
 
 
+# MongoDB Connection Details
+MONGO_HOST="mongodb+srv://library:saimohanlib@cluster0.k1eoilz.mongodb.net/online-library"
+DATABASE_NAME="online-library"
+COLLECTION_NAME="reports"
 
+# Email Details
+RECIPIENT="krishnakakarapathi@gmail.com"
+SENDER="kakarapathikrishna@gmail.com"
+SUBJECT="Container Scanning Report"
+BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
 
+# File Paths
+OUTPUT_FILE="container_scanning_report.txt"
+ATTACHMENT="container_scanning_report.txt"
 
+# Connect to MongoDB and retrieve record
+mongosh --host $MONGO_HOST --eval "db.$COLLECTION_NAME.find().sort({ timestampField: -1 }).limit(1)" > $OUTPUT_FILE
 
+# Send email with attachment
+echo "$BODY" | mailx -s "$SUBJECT" -a "$ATTACHMENT" -r "$SENDER" "$RECIPIENT"
 
-# # Define email content and recipients
-# EMAIL_SUBJECT="Container Scanning Report"
-
-# RECIPIENT="krishnakakarapathi@gmail.com"
-
-# #  curl --location --header "PRIVATE-TOKEN: 9koXpg98eAheJpvBs5tK" "https://gitlab.example.com/api/v4/projects/1/jobs/8/artifacts"
-
-# # Download the artifact using wget
-# wget -O container-scanning-report.json "https://gitlab.com/gitlabworkflow/online-library/-/jobs/6363017609/artifacts/raw/container-scanning-report.json"
-
-# # Construct the email message
-# EMAIL_BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
-
-# # Send email with attachment
-# echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" "$RECIPIENT" -a "container-scanning-report.json"
+# Clean up temporary files
+rm $OUTPUT_FILE
 
 
