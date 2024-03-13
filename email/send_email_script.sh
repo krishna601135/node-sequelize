28337c07189a3eecfb0527682b1417e47fe73258
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-13 14:49:49 +0530

    adding smtp to the file location

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -18,18 +18,29 @@ BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\
 # File Paths
 OUTPUT_FILE="container_scanning_report.txt"
 ATTACHMENT="container_scanning_report.txt"
-SMTP_SERVER="smtp.gmail.com"
+
 
 # export MONGO_PASSWORD=""
 
 # Connect to MongoDB and retrieve record
 mongosh "mongodb+srv://library:saimohanlib@cluster0.k1eoilz.mongodb.net/online-library" --apiVersion 1 --username library --password saimohanlib --eval "db.$COLLECTION_NAME.find().sort({ timestampField: -1 }).limit(1)" > $OUTPUT_FILE
 
+# Set SMTP server address
+SMTP_SERVER="smtp.gmail.com"
 
+# Edit mail configuration file and add SMTP server setting
+if [ -f "/etc/mail.rc" ]; then
+    sudo sed -i "1i set smtp=smtp://$SMTP_SERVER" /etc/mail.rc
+elif [ -f "$HOME/.mailrc" ]; then
+    echo "set smtp=smtp://$SMTP_SERVER" >> "$HOME/.mailrc"
+else
+    echo "Mail configuration file not found."
+    exit 1
+fi
 
 # Send email with attachment
 # echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER $RECIPIENT
-echo "$BODY" | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER -S smtp=$SMTP_SERVER $RECIPIENT
+echo "$BODY" | mail -s "$SUBJECT" -a "$OUTPUT_FILE" -r "$SENDER" "$RECIPIENT"
 
 
 #$? contains the exit status of the last command.
