88772092587850cfc82b7b80a9ce2711b529597e
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-14 14:33:34 +0530

    changed the position of smtp

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -32,7 +32,7 @@ SMTP_SERVER="smtp.gmail.com"
 
 # Send email with attachment
 # echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER $RECIPIENT
-echo $BODY | mailx -S smtp=$SMTP_SERVER -s $SUBJECT krishnakakarapathi@gmail.com -a $OUTPUT_FILE 
+echo $BODY | mailx -s $SUBJECT -S smtp=mailrelay.domain.com:25 krishnakakarapathi@gmail.com -a $OUTPUT_FILE 
 
 
 #$? contains the exit status of the last command.
