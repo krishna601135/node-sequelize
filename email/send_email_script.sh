61e16912cf76f2fd13a40ffe991882535abd4017
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-14 17:11:25 +0530

    changes added in three files

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -14,7 +14,7 @@ COLLECTION_NAME="reports"
 # Email Details
 RECIPIENT="krishnakakarapathi@gmail.com"
 SENDER="kakarapathikrishna@gmail.com"
-SUBJECT="Container Scanning Report"
+MAIL_SUBJECT="Container Scanning Report"
 BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
 
 # File Paths
@@ -34,15 +34,28 @@ SMTP_SERVER="smtp.gmail.com"
 
 # Send email with attachment
 # echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER $RECIPIENT
-echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE krishnakakarapathi@gmail.com
-
-
-#$? contains the exit status of the last command.
+echo $BODY | mail -s $MAIL_SUBJECT -a $OUTPUT_FILE krishnakakarapathi@gmail.com
 if [ $? -eq 0 ]; then
     echo "Email sent successfully"
 else
     echo "Failed to send email"
 fi
+
+cat /var/log/maillog
+# mailx -v -s "$EMAIL_SUBJECT" \
+# -S smtp-use-starttls \
+# -S ssl-verify=ignore \
+# -S smtp-auth=login \
+# -S smtp=smtp://smtp.gmail.com:587 \
+# -S from="$FROM_EMAIL_ADDRESS($FRIENDLY_NAME)" \
+# -S smtp-auth-user=$FROM_EMAIL_ADDRESS \
+# -S smtp-auth-password=$EMAIL_ACCOUNT_PASSWORD \
+# -S ssl-verify=ignore \
+# -S nss-config-dir=~/.mozilla/firefox/xxxxxxxx.default/ \
+# $TO_EMAIL_ADDRESS
+
+
+#$? contains the exit status of the last command.
 # Clean up temporary files
 
 
