57dfc8ed66795abb600e3283e3e590c20d2b0149
krishnabhimavaram <krishnakakarapathi@gmail.com>
2024-03-18 16:23:01 +0530

    testing mutt instead of mailx

--- email/send_email_script.sh
+++ email/send_email_script.sh
@@ -7,44 +7,50 @@
 ####################
 
 
-# MongoDB Connection Details
-DATABASE_NAME="online-library"
-COLLECTION_NAME="reports"
+# # MongoDB Connection Details
+# DATABASE_NAME="online-library"
+# COLLECTION_NAME="reports"
 
-# Email Details
-RECIPIENT="krishnakakarapathi@gmail.com"
-SENDER="kakarapathikrishna@gmail.com"
-MAIL_SUBJECT="Container Scanning Report"
-BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
+# # Email Details
+# RECIPIENT="krishnakakarapathi@gmail.com"
+# SENDER="kakarapathikrishna@gmail.com"
+# MAIL_SUBJECT="Container Scanning Report"
+# BODY="Hello,\n\nPlease find attached the container scanning report.\n\nRegards,\nYour Team"
 
-# File Paths
-OUTPUT_FILE="container_scanning_report.txt"
-ATTACHMENT="container_scanning_report.txt"
-
-
-
-# Connect to MongoDB and retrieve record
-mongosh "mongodb+srv://library:saimohanlib@cluster0.k1eoilz.mongodb.net/online-library" --apiVersion 1 --username library --password saimohanlib --eval "db.$COLLECTION_NAME.find().sort({ timestampField: -1 }).limit(1)" > $OUTPUT_FILE
-
-#cat $OUTPUT_FILE
-
-# Set SMTP server address
-SMTP_SERVER="smtp.gmail.com"
+# # File Paths
+# OUTPUT_FILE="container_scanning_report.txt"
+# ATTACHMENT="container_scanning_report.txt"
 
 
-# Send email with attachment
-# echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER $RECIPIENT
-echo $BODY | mail -s $MAIL_SUBJECT -A $OUTPUT_FILE krishnakakarapathi@gmail.com
 
-#$? contains the exit status of the last command.
+# # Connect to MongoDB and retrieve record
+# mongosh "mongodb+srv://library:saimohanlib@cluster0.k1eoilz.mongodb.net/online-library" --apiVersion 1 --username library --password saimohanlib --eval "db.$COLLECTION_NAME.find().sort({ timestampField: -1 }).limit(1)" > $OUTPUT_FILE
+
+# #cat $OUTPUT_FILE
+
+# # Set SMTP server address
+# SMTP_SERVER="smtp.gmail.com"
+
+
+# # Send email with attachment
+# # echo $BODY | mail -s $SUBJECT -a $OUTPUT_FILE -r $SENDER $RECIPIENT
+# echo $BODY | mail -s "$MAIL_SUBJECT" -A $OUTPUT_FILE krishnakakarapathi@gmail.com
+
+# #$? contains the exit status of the last command.
+# if [ $? -eq 0 ]; then
+#     echo "Email sent successfully"
+# else
+#     echo "Failed to send email"
+# fi
+
+
+# # Clean up temporary files
+# rm $OUTPUT_FILE
+
+
+mutt -s "Test Email" krishnakakarapathi@gmail.com
 if [ $? -eq 0 ]; then
     echo "Email sent successfully"
 else
     echo "Failed to send email"
 fi
-
-
-# Clean up temporary files
-rm $OUTPUT_FILE
-
-
